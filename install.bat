824ef22589215bbdaf4ae26a97f4518bf591f955
Kensaku Tanaka <tannakaken@gmail.com>
2026-02-13 14:14:45 +0900

    テスト

--- install.bat
+++ install.bat
@@ -4,6 +4,10 @@ echo  ファイル名0埋め変換メニュー登録ツール
 echo ==========================================
 echo.
 
+REM "スクリプトの絶対パスを取得"
+set SCRIPT_PATH=%~dp0rename-tool.ps1
+echo %SCRIPT_PATH%
+
 REM "管理者権限チェック"
 net session >nul 2>&1
 if %errorlevel% neq 0 (
@@ -13,13 +17,10 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-REM "スクリプトの絶対パスを取得"
-set SCRIPT_PATH=%~dp0rename-tool.ps1
-
 REM "レジストリ登録"
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyZeroPadding" /ve /d "ファイル名0埋め変換" /f
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyZeroPadding" /v "Icon" /d "powershell.exe" /f
-reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyZeroPadding\command" /ve /d "powershell.exe -ExecutionPolicy Bypass -NoProfile -File \"%SCRIPT_PATH%\" \"%%V\"" /f
+rem reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyZeroPadding" /ve /d "ファイル名0埋め変換" /f
+rem reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyZeroPadding" /v "Icon" /d "powershell.exe" /f
+rem reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\MyZeroPadding\command" /ve /d "powershell.exe -ExecutionPolicy Bypass -NoProfile -File \"%SCRIPT_PATH%\" \"%%V\"" /f
 
 echo.
 echo [完了] 右クリックメニューに登録されました
